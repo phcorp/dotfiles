b8abdba05c8865079d0d3f63915f67b718ecf0ef
Pierre Hennequart <pierre@janalis.com>
2019-09-16 16:19:00 +0200

    add tsplit command

--- .sh/functions.sh
+++ .sh/functions.sh
@@ -13,6 +13,18 @@ function safe_run() {
   fi
 }
 
+# split terminal
+function tsplit() {
+	TSPLIT_WINDOWS=${1:-2}
+	COMMAND="tmux new-session \"tmux new-window \;"
+	for TSPLIT_WINDOW in `seq 2 $TSPLIT_WINDOWS`
+	do
+		COMMAND="$COMMAND split-window -d \;"
+	done
+	COMMAND="$COMMAND next-layout \; set-window-option synchronize-panes on\""
+	eval $COMMAND
+}
+
 # print all available colors
 function color_palette {
 	for clbg in {40..47} {100..107} 49 ; do
@@ -39,6 +51,11 @@ function symfony {
 	    return;
 	fi
     echo "${color_danger}Symfony console script was not found${color_reset}" >&2;
+}
+
+# use ssh key
+function ssh-use-key {
+	eval "$(ssh-agent -s)" && ssh-add -K "$*"
 }
 
 # rebase upstream
